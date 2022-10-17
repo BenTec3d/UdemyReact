93b69bbd5f581d869c2187b620e6f0001e477471
BenTec <bentec3d@gmail.com>
2022-10-17 15:45:22 +0200

    managing showCart state to show/hide the Cart

--- PracticeProject_3/src/App.js
+++ PracticeProject_3/src/App.js
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import Cart from "./components/Cart/Cart";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals"
 
 const App = () => {
+  const [showCart, setShowCart] = useState(false);
+
+  const openCartHandler = () => {
+    setShowCart(true);
+  };
+
+  const closeCartHandler = () => {
+    setShowCart(false);
+  };
+
   return (
     <React.Fragment>
-      <Cart />
-      <Header />
+      {showCart && <Cart onClose={closeCartHandler} />}
+      <Header onOpenCart={openCartHandler} />
       <main>
         <Meals />
       </main>
