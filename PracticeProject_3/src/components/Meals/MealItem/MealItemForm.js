86f38bff21e5c0cf60776644223448f45810cb49
BenTec <bentec3d@gmail.com>
2022-10-17 12:53:00 +0200

    added id prop to input

--- PracticeProject_3/src/components/Meals/MealItem/MealItemForm.js
+++ PracticeProject_3/src/components/Meals/MealItem/MealItemForm.js
@@ -4,14 +4,16 @@ import classes from "./MealItemForm.module.css";
 const MealItemForm = props => {
     return (
         <form className={classes.form}>
-            <Input label="Amount" input={{
-                id: "amount",
-                type: "number",
-                min: "1",
-                max: "9",
-                step: "1",
-                defaultValue: "1"
-            }} />
+            <Input
+                label="Amount"
+                input={{
+                    id: "amount_" + props.id,
+                    type: "number",
+                    min: "1",
+                    max: "9",
+                    step: "1",
+                    defaultValue: "1"
+                }} />
             <button>Add</button>
         </form>
     );
