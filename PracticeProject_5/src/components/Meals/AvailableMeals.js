60c3efb031e1b6696cf72bb6de06226262958616
BenTec <bentec3d@gmail.com>
2022-10-19 16:26:59 +0200

    replaced dummy meals with fetched meals from API

--- PracticeProject_5/src/components/Meals/AvailableMeals.js
+++ PracticeProject_5/src/components/Meals/AvailableMeals.js
@@ -1,43 +1,38 @@
+import { useState, useEffect } from "react";
+
 import Card from "../UI/Card";
 import classes from "./AvailableMeals.module.css";
 import MealItem from "./MealItem/MealItem";
 
-const DUMMY_MEALS = [
-    {
-        id: 'm1',
-        name: 'Sushi',
-        description: 'Finest fish and veggies',
-        price: 22.99,
-    },
-    {
-        id: 'm2',
-        name: 'Schnitzel',
-        description: 'A german specialty!',
-        price: 16.5,
-    },
-    {
-        id: 'm3',
-        name: 'Barbecue Burger',
-        description: 'American, raw, meaty',
-        price: 12.99,
-    },
-    {
-        id: 'm4',
-        name: 'Green Bowl',
-        description: 'Healthy...and green...',
-        price: 18.99,
-    },
-];
-
 const AvailableMeals = () => {
-    const mealsList = DUMMY_MEALS.map(meal =>
-        <MealItem
-            key={meal.id}
-            id={meal.id}
-            name={meal.name}
-            description={meal.description}
-            price={meal.price} />
-    );
+    const [mealsList, setMealsList] = useState([]);
+
+    useEffect(() => {
+        const fetchMeals = async () => {
+            const response = await fetch("https://udemyreact-1714c-default-rtdb.europe-west1.firebasedatabase.app/meals.json");
+            const responseData = await response.json();
+
+            const fetchedMeals = [];
+
+            for (const key in responseData) {
+                fetchedMeals.push({
+                    id: key,
+                    ...responseData[key]
+                });
+            }
+
+            setMealsList(fetchedMeals.map(meal =>
+                <MealItem
+                    key={meal.id}
+                    id={meal.id}
+                    name={meal.name}
+                    description={meal.description}
+                    price={meal.price} />
+            ));
+        }
+
+        fetchMeals();
+    }, []);
 
     return (
         <section className={classes.meals}>
