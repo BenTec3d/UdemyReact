21228f6fc95eef27983f25f0b2bf376ea3c525d4
BenTec <bentec3d@gmail.com>
2022-10-19 14:37:02 +0200

    added second solution using useReducer

--- PracticeProject_4/src/hooks/useInput.js
+++ PracticeProject_4/src/hooks/useInput.js
@@ -1,26 +1,79 @@
-import { useState } from "react";
+//Solution 1 using useState
+
+// import { useState } from "react";
+
+// const useInput = (validation) => {
+//     const [enteredValue, setEnteredValue] = useState('');
+//     const [isBlurred, setIsBlurred] = useState(false);
+
+//     const isValid = validation(enteredValue);
+//     const hasError = !isValid && isBlurred;
+
+//     const valueChangedHandler = (event) => {
+//         setIsBlurred(false);
+//         setEnteredValue(event.target.value);
+//     };
+//     const blurChangedHandler = () => {
+//         setIsBlurred(true);
+//     };
+//     const reset = () => {
+//         setEnteredValue("");
+//         setIsBlurred(false);
+//     };
+
+//     return {
+//         value: enteredValue,
+//         isValid,
+//         hasError,
+//         valueChangedHandler,
+//         blurChangedHandler,
+//         reset
+//     }
+// };
+
+//export default useInput;
+
+
+//Solution 2 using useReducer
+
+import { useReducer } from "react";
+
+const defaultState = {
+    value: "",
+    isBlurred: false
+};
+
+const reducer = (state, action) => {
+    switch(action.type){
+        case "VALUE":
+            return {value: action.value, isBlurred: false};
+        case "BLUR":
+            return {value: state.value, isBlurred: true};
+        case "RESET":
+            return {value: "", isBlurred: false};
+        default:
+            return defaultState;
+    }
+}
 
 const useInput = (validation) => {
-    const [enteredValue, setEnteredValue] = useState('');
-    const [isBlurred, setIsBlurred] = useState(false);
+    const [inputState, dispatch] = useReducer(reducer, defaultState);
 
-    const isValid = validation(enteredValue);
-    const hasError = !isValid && isBlurred;
+    const isValid = validation(inputState.value);
+    const hasError = !isValid && inputState.isBlurred;
 
     const valueChangedHandler = (event) => {
-        setIsBlurred(false);
-        setEnteredValue(event.target.value);
+        dispatch({type: "VALUE", value: event.target.value});
     };
     const blurChangedHandler = () => {
-        setIsBlurred(true);
+        dispatch({type: "BLUR"});
     };
     const reset = () => {
-        setEnteredValue("");
-        setIsBlurred(false);
+        dispatch({type: "RESET"});
     };
 
     return {
-        value: enteredValue,
+        value: inputState.value,
         isValid,
         hasError,
         valueChangedHandler,
