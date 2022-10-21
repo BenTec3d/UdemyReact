41394339ecdd2e57d921bf34c4918b853b655bf8
BenTec <bentec3d@gmail.com>
2022-10-21 19:50:16 +0200

    added visibility toggle between Cart and Products

--- PracticeProject_6/src/App.js
+++ PracticeProject_6/src/App.js
@@ -1,12 +1,16 @@
+import { useSelector } from 'react-redux';
+
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 
 function App() {
+  const cartIsVisible = useSelector((state) => state.cart.isVisible)
+
   return (
     <Layout>
-      <Cart />
-      <Products />
+      {cartIsVisible && <Cart />}
+      {!cartIsVisible && <Products />}
     </Layout>
   );
 }
