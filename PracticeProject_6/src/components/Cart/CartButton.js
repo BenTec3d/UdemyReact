925d0b8a2179cfe11528686e25ea215c01e03a20
BenTec <bentec3d@gmail.com>
2022-10-21 19:51:25 +0200

    made badge dynamic and added visibility toggle

--- PracticeProject_6/src/components/Cart/CartButton.js
+++ PracticeProject_6/src/components/Cart/CartButton.js
@@ -1,10 +1,22 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { cartActions } from '../../store/cartSlice';
 import classes from './CartButton.module.css';
 
 const CartButton = (props) => {
+  const items = useSelector((state) => state.cart.items)
+  let itemCount = 0;
+  items.map(item => itemCount += item.quantity);
+
+  const dispatch = useDispatch();
+
+  const onShowCartHandler = () => {
+    dispatch(cartActions.toggleVisiblity());
+  };
+
   return (
-    <button className={classes.button}>
+    <button onClick={onShowCartHandler} className={classes.button}>
       <span>My Cart</span>
-      <span className={classes.badge}>1</span>
+      <span className={classes.badge}>{itemCount}</span>
     </button>
   );
 };
