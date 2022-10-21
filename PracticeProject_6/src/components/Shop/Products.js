10b8ed11c7d84e08341bf96a870aa5ffda407bfe
BenTec <bentec3d@gmail.com>
2022-10-21 20:32:01 +0200

    refractored to map dummy data

--- PracticeProject_6/src/components/Shop/Products.js
+++ PracticeProject_6/src/components/Shop/Products.js
@@ -1,21 +1,35 @@
 import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
+const dummyProducts = [
+  {
+    id: "p1",
+    title: "RTX 4090",
+    price: 2200,
+    description: "Expensive!"
+  }, {
+    id: "p2",
+    title: "i9 13900k",
+    price: 650,
+    description: "Hot!"
+  },
+];
+
+const productItems = dummyProducts.map(item =>
+  <ProductItem
+    id={item.id}
+    key={item.id}
+    title={item.title}
+    price={item.price}
+    description={item.description} />
+);
+
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='RTX 4090'
-          price={2200}
-          description='Expensive!'
-        />
-        <ProductItem
-          title='i9 13900k'
-          price={650}
-          description='Hot!'
-        />
+        {productItems}
       </ul>
     </section>
   );
