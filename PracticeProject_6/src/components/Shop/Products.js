0d37b508c2a116b957a231b1c3bbdf0c4910da2b
BenTec <bentec3d@gmail.com>
2022-10-21 20:04:42 +0200

    added second dummy product

--- PracticeProject_6/src/components/Shop/Products.js
+++ PracticeProject_6/src/components/Shop/Products.js
@@ -7,9 +7,14 @@ const Products = (props) => {
       <h2>Buy your favorite products</h2>
       <ul>
         <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
+          title='RTX 4090'
+          price={2200}
+          description='Expensive!'
+        />
+        <ProductItem
+          title='i9 13900k'
+          price={650}
+          description='Hot!'
         />
       </ul>
     </section>
